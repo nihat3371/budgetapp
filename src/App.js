6f5e4523f387d6ab282de00518917976997eae3d
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-05-23 14:20:03 +0300

    budget ui started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Button, Container, Stack } from "react-bootstrap";
+import BudgetCard from "./components/BudgetCard";
+
 function App() {
-  return null;
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div>
+        <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
